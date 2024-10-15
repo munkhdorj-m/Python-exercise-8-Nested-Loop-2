12406c9ea8c06c63703558b895fed7fcc92effff
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2024-10-15 12:26:45 +0800

    Update test_assignment.py

--- test_assignment.py
+++ test_assignment.py
@@ -8,24 +8,36 @@ def check_contains_loop(function):
 
 def test1(capsys):
     expected_output = (
-        "1 2 3 4 5 \n"
-        "1 2 3 4 \n"
-        "1 2 3 \n"
-        "1 2 \n"
         "1 \n"
+        "2 2 \n"
+        "3 3 3 \n"
+        "4 4 4 4 \n"
+        "5 5 5 5 5 \n"
     )
     print_pattern_1(5)
     captured = capsys.readouterr()
     assert captured.out == expected_output
 
 def test2(capsys):
-    expected_output = "1 \n2 3 \n4 5 6 \n7 8 9 10 \n11 12 13 14 15 \n" 
+    expected_output = (
+        "    1 \n"
+        "   2 2 \n"
+        "  3 3 3 \n"
+        " 4 4 4 4 \n"
+        "5 5 5 5 5 \n"
+    )
     print_pattern_2(5)
     captured = capsys.readouterr()
     assert captured.out == expected_output
 
 def test3(capsys):
-    expected_output = "    *\n   **\n  ***\n ****\n*****\n"
+    expected_output = (
+        "1 \n"
+        "0 1 \n"
+        "1 0 1 \n"
+        "0 1 0 1 \n"
+        "1 0 1 0 1 \n"
+    )
     print_pattern_3(5)
     captured = capsys.readouterr()
     assert captured.out == expected_output
